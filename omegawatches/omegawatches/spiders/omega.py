0acbb598edaf4bb470120877bdd0f64fe173ab42
Peter Faso <quinlayen@gmail.com>
2023-08-02 02:39:04 -1000

    refactoring pagination.  sending to remote

--- omegawatches/omegawatches/spiders/omega.py
+++ omegawatches/omegawatches/spiders/omega.py
@@ -18,42 +18,43 @@ class OmegaSpider(CrawlSpider):
     name = "omega"
     allowed_domains = ["omegawatches.com"]
     # start_urls = ["https://www.omegawatches.com/en-us/watchfinder?p=2"]
-    start_urls = ["https://www.omegawatches.com/en-us/watchfinder"]
+    start_urls = [f"https://www.omegawatches.com/en-us/watchfinder?p={i}" for i in range(1,73)]
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=('//ol[@class="products list items product-items"]//a')), callback="parse_item"),
+        Rule(LinkExtractor(deny='watchfinder')),
     )
+    # rules = (
+    #     Rule(LinkExtractor(restrict_xpaths=('//ol[@class="products list items product-items"]//a')), callback="parse_item"),
+    # )
     
-    def parse_item(self, response):
+    # def parse_item(self, response):
 
-      
-        loader = ItemLoader(item=WatchItem(), response=response)
-  
-        features_list = response.xpath('//div[@class="pm-feature-tooltip"]/p/span/text()').getall()
+    #     loader = ItemLoader(item=WatchItem(), response=response)
+    #     features_list = response.xpath('//div[@class="pm-feature-tooltip"]/p/span/text()').getall()
 
         
-        loader.add_value('watch_url', response.url)
-        loader.add_xpath('parent_model','//span[@class="product attribute collection hidden"]/text()')
-        loader.add_xpath('specific_model','//span[@class="product attribute subcollection"]/text()')
-        loader.add_xpath('nickname','//span[@class="product attribute name"]/text()')
-        loader.add_xpath('sku','//p[@class="product attribute sku"]/span/text()')
-        loader.add_xpath('description','//*[@id="product-info-description"]/div/div/p/text()')
-        loader.add_xpath('price','//span[@class="price"]/text()')
-        loader.add_xpath('case_material','//span[@data-code="watch_watchcase"]/text()')
-        loader.add_xpath('case_diameter','//span[@data-code="watch_casediameter"]/text()')
-        loader.add_xpath('between_lugs','//span[@data-code="watch_between_lugs_size"]/text()')
-        loader.add_xpath('case_thickness','//span[@data-code="watch_thickness"]/text()')
-        loader.add_xpath('lug_to_lug','//span[@data-code="watch_lug_to_lug"]/text()')
-        loader.add_xpath('weight','//span[@data-code="weight"]/text()')
-        loader.add_xpath('water_resistance','//span[@data-code="watch_waterresistance"]/text()')
-        loader.add_xpath('crystal','//span[@data-code="watch_crystal"]/text()')
-        loader.add_xpath('bracelet_material','//span[@data-code="watch_bracelet"]/text()')
-        loader.add_xpath('clasp_type','//span[@data-code="strap_clasp_type"]/text()')
-        loader.add_value('features', features_list)
-        loader.add_xpath('power_reserve','normalize-space(//li[@class="ow-mod_37__picto ow-mod_37__picto--power-reserve"]/span/text())')
-        # loader.add_xpath('caliber','//div[@class="ow-mod__col-content"]/h2/span[@class="pm-title"]/span[2]/text()')
-        # loader.add_xpath('dial_color','//span[@data-code="watch_dial" ]/text()')
-        yield loader.load_item()
+    #     loader.add_value('watch_url', response.url)
+    #     loader.add_xpath('parent_model','//span[@class="product attribute collection hidden"]/text()')
+    #     loader.add_xpath('specific_model','//span[@class="product attribute subcollection"]/text()')
+    #     loader.add_xpath('nickname','//span[@class="product attribute name"]/text()')
+    #     loader.add_xpath('sku','//p[@class="product attribute sku"]/span/text()')
+    #     loader.add_xpath('description','//*[@id="product-info-description"]/div/div/p/text()')
+    #     loader.add_xpath('price','//span[@class="price"]/text()')
+    #     loader.add_xpath('case_material','//span[@data-code="watch_watchcase"]/text()')
+    #     loader.add_xpath('case_diameter','//span[@data-code="watch_casediameter"]/text()')
+    #     loader.add_xpath('between_lugs','//span[@data-code="watch_between_lugs_size"]/text()')
+    #     loader.add_xpath('case_thickness','//span[@data-code="watch_thickness"]/text()')
+    #     loader.add_xpath('lug_to_lug','//span[@data-code="watch_lug_to_lug"]/text()')
+    #     loader.add_xpath('weight','//span[@data-code="weight"]/text()')
+    #     loader.add_xpath('water_resistance','//span[@data-code="watch_waterresistance"]/text()')
+    #     loader.add_xpath('crystal','//span[@data-code="watch_crystal"]/text()')
+    #     loader.add_xpath('bracelet_material','//span[@data-code="watch_bracelet"]/text()')
+    #     loader.add_xpath('clasp_type','//span[@data-code="strap_clasp_type"]/text()')
+    #     loader.add_value('features', features_list)
+    #     loader.add_xpath('power_reserve','normalize-space(//li[@class="ow-mod_37__picto ow-mod_37__picto--power-reserve"]/span/text())')
+    #     # loader.add_xpath('caliber','//div[@class="ow-mod__col-content"]/h2/span[@class="pm-title"]/span[2]/text()')
+    #     # loader.add_xpath('dial_color','//span[@data-code="watch_dial" ]/text()')
+    #     yield loader.load_item()
 
       
  
