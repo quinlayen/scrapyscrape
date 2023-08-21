db51372d3e0d481116ce3fd4277417c80e1c1851
Peter Faso <quinlayen@gmail.com>
2023-08-21 02:11:12 -1000

    Filled in all fields. Some need actual values

--- omegawatches/omegawatches/items.py
+++ omegawatches/omegawatches/items.py
@@ -9,12 +9,26 @@ import scrapy
    
 class WatchItem(scrapy.Item):
 
+    brand = scrapy.Field()
+    style = scrapy.Field()
+    year_introduced = scrapy.Field()
+    case_finish = scrapy.Field()
+    caseback = scrapy.Field()
+    bezel_material = scrapy.Field()
+    bezel_color = scrapy.Field()
+    numerals = scrapy.Field()
+    bracelet_color = scrapy.Field()
+    movement = scrapy.Field()
+    frequency = scrapy.Field()
+    jewels = scrapy.Field()
     watch_url = scrapy.Field()
     parent_model = scrapy.Field()
     specific_model = scrapy.Field()
     nickname = scrapy.Field()
+    marketing_name = scrapy.Field()
     sku = scrapy.Field()
     description = scrapy.Field()
+    currency = scrapy.Field()
     price = scrapy.Field()
     case_material = scrapy.Field()
     case_diameter = scrapy.Field()
@@ -31,8 +45,8 @@ class WatchItem(scrapy.Item):
     battery_life = scrapy.Field()
     caliber = scrapy.Field()
     features = scrapy.Field()
-    # image_urls = scrapy.Field()
-    # images = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
     
     
     # from scrapy.loader.processors import TakeFirst, MapCompose, Join, Identity
--- omegawatches/omegawatches/spiders/omega.py
+++ omegawatches/omegawatches/spiders/omega.py
@@ -5,11 +5,14 @@ from ..items import WatchItem
 from ..itemloaders import OmegaItemLoader
 import scrapy
 from scrapy.loader import ItemLoader
+from scrapy import Selector
 import os
 import json
 from w3lib.html import remove_tags
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+# from scrapy.selector import HtmlXPathSelector
+# from scrapy.http import HtmlResponse
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv('SCRAPFLY_API_KEY')
@@ -19,36 +22,57 @@ class OmegaSpider(CrawlSpider):
     name = "omega"
     allowed_domains = ["omegawatches.com"]
     start_urls = ["https://www.omegawatches.com/en-us/watchfinder?p=5"]
-    # start_urls = [f"https://www.omegawatches.com/en-us/watchfinder?p={i}" for i in range(1,73)]
+    # start_urls = ["https://www.omegawatches.com/en-us/watchfinder?filter_movement_type=Q"]
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths=('//ol[@class="products list items product-items"]//a')), callback="parse_item"),
     )
     
     def parse_item(self, response):
-        
-        
-        features_list = response.xpath('//div[@class="pm-feature-tooltip"]/p/span/text()').getall()
-
-
-        # Cycle through to change path and field names depending on whether battery powered or mechanical
-        if 'quartz' in response.url:
-            power_field_name = 'battery_life'
-            power_xpath = '//div[@class="product-info-data-content movement watches"]//li[3]/span/text()'
-            caliber_xpath = '//div[@class="product-info-data-content movement watches"]//li[1]/span/text()'
-        else:
-            power_field_name = 'power_reserve' 
-            power_xpath = 'normalize-space(//li[@class="ow-mod_37__picto ow-mod_37__picto--power-reserve"]/span/text())'
-            caliber_xpath = '//div[@class="ow-mod__col-content"]/h2/span[@class="pm-title"]/span[2]/text()'
-            
-        # loader = ItemLoader(item=WatchItem(), response=response)
         loader = OmegaItemLoader(item=WatchItem(), response=response)
+
+        
+        features_li_tags = response.xpath('//div[@class="product-info-data-content features watches"]/ul/li')
+        # for feature_text in features_li_tags.xpath('.//span/text()').extract()
+       
+ 
+        if 'quartz' in response.url:
+            # battery_life_xpath = '//div[@class="product-info-data-content movement watches"]//li[3]/span/text()'
+            caliber_xpath = '//div[@class="product-info-data-content movement watches"]//li[1]/span/text()'
+            movement_xpath = '//div[@class="product-info-data-content movement watches"]//li[4]/span/text()'
+        else:
+            # power_reserve_xpath = 'normalize-space(//ul[@class="ow-mod_37__pictos"]/li[1]/span/text())'
+            caliber_xpath = '//div[@class="ow-mod__col-content"]/h2/span[@class="pm-title"]/span[2]/text()'
+            movement_xpath = 'normalize-space(//ul[@class="ow-mod_37__pictos"]/li[2]/span/text())'
+            # frequency_xpath = 'normalize-space(//ul[@class="ow-mod_37__pictos"]/li[3]/span/text())'
+   
+        #####   
+        for li_tag in features_li_tags:
+            features_text = li_tag.xpath('.//span/text()').get()
+            
+            loader.add_value('features', features_text)
+            
+        loader.add_value('brand', 'Omega')
+        loader.add_value('style', ' ')
+        loader.add_value('year_introduced', ' ')
+        loader.add_value('case_finish', ' ')
+        loader.add_xpath('caseback', '//*[@id="product-info-description"]/div/div/p/text()')
+        loader.add_xpath('bezel_material', '//*[@id="product-info-description"]/div/div/p/text()')
+        loader.add_value('bezel_color', ' ')
+        loader.add_xpath('numerals', '//*[@id="product-info-description"]/div/div/p/text()')
+        loader.add_xpath('bracelet_color', '//*[@id="tab-strap"]/ul/li[2]/span/text()')
+        loader.add_value('jewels', ' ')
+        loader.add_value('year_introduced', ' ')
+        loader.add_xpath('numerals', '//*[@id="product-info-description"]/div/div/p/text()')
+        loader.add_xpath('bracelet_color', '//*[@id="tab-strap"]/ul/li[2]/span/text()')
+        loader.add_xpath('marketing_name', '//span[contains (@class, "product attribute marketing-name")]/text()')
         loader.add_value('watch_url', response.url)
         loader.add_xpath('parent_model','//span[@class="product attribute collection hidden"]/text()')
         loader.add_xpath('specific_model','//span[@class="product attribute subcollection"]/text()')
         loader.add_xpath('nickname','//span[@class="product attribute name"]/text()')
         loader.add_xpath('sku','//p[@class="product attribute sku"]/span/text()')
         loader.add_xpath('description','//*[@id="product-info-description"]/div/div/p/text()')
+        loader.add_value('currency','USD')
         loader.add_xpath('price','//span[@class="price"]/text()')
         loader.add_xpath('case_material','//span[@data-code="watch_watchcase"]/text()')
         loader.add_xpath('case_diameter','//span[@data-code="watch_casediameter"]/text()')
@@ -60,9 +84,11 @@ class OmegaSpider(CrawlSpider):
         loader.add_xpath('crystal','//span[@data-code="watch_crystal"]/text()')
         loader.add_xpath('bracelet_material','//span[@data-code="watch_bracelet"]/text()')
         loader.add_xpath('clasp_type','//span[@data-code="strap_clasp_type"]/text()')
-        loader.add_value('features', features_list)
-        loader.add_xpath(f'{power_field_name}', f'{power_xpath}')
-        loader.add_xpath('caliber', f'{caliber_xpath}')
+        loader.add_xpath('frequency', 'normalize-space(//ul[@class="ow-mod_37__pictos"]/li[3]/span/text())')
+        loader.add_xpath('movement', movement_xpath)
+        loader.add_xpath('caliber', caliber_xpath)
+        loader.add_xpath('power_reserve', 'normalize-space(//ul[@class="ow-mod_37__pictos"]/li[1]/span/text())')
+        loader.add_xpath('battery_life', '//div[@class="product-info-data-content movement watches"]//li[3]/span/text()')
         loader.add_xpath('dial_color','//span[@data-code="watch_dial" ]/text()')
         # loader.add_xpath('image_urls','//*[@id="gallery-frame-0"]/img[2]/@src')
         yield loader.load_item()
